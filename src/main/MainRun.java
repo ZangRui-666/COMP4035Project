1d6bc3b9db83455a621806c5ba4ce129bd200882
tyrannnnnt <58685747+tyrannnnnt@users.noreply.github.com>
2022-11-12 03:15:46 +0800

    fix filename with space

--- src/main/MainRun.java
+++ src/main/MainRun.java
@@ -16,21 +16,23 @@ public class MainRun {
         System.out.print("> ");
         while (!quit) {
             if (in.hasNextLine()) {
-                String[] input = in.nextLine().split(" ");
-                if (input.length == 2) {
-                    switch (input[0]) {
+                String inputCommand = in.nextLine();
+                String[] input = inputCommand.split(" ");
+                if(input.length > 1){
+                    switch (input[0]){
                         case "btree":
-                            switch (input[1]) {
+                            String remainStr = inputCommand.substring(input[0].length()+1);
+                            System.out.println("This is the remian Str :" +remainStr);
+                            switch (remainStr) {
                                 case "-help":
                                     System.out.println("Usage: btree [fname]");
                                     System.out.println("fname: the name of the data file storing the search key values");
                                     break;
                                 default:
                                     try {
-                                        File inputFile = new File(input[1]);
+                                        File inputFile = new File(remainStr);
                                         if (!inputFile.exists())
                                             System.out.println("The input path does not exist, please check!");
-                                            //todo: add btree build function
                                         else {
                                             System.out.println("Building an initial B+-tree... Launching the B+-tree test program...");
                                             tree = new BPlusTree(inputFile);
@@ -39,20 +41,34 @@ public class MainRun {
                                     } catch (FileNotFoundException e) {
                                         System.out.println(e.getMessage());
                                     }
-
                             }
                             break;
                         case "insert":
+                            if(input.length != 2){
+                                System.out.println("Invalid input format.");
+                                return;
+                            }
                             tree.put(input[1], 0);
                             System.out.println("The key " + input[1] + " has been inserted in the B+-tree!");
                             break;
                         case "delete":
+                            if(input.length != 2){
+                                System.out.println("Invalid input format.");
+                                return;
+                            }
                             tree.Remove(input[1]);
+                            break;
+                        case"search":
+                            if(input.length != 3){
+                                System.out.println("Invalid input format.");
+                                return;
+                            }
+                            System.out.println(tree.Search(input[1], input[2]));
                             break;
                         default:
                             System.out.println("Invalid input format.");
                     }
-                } else if (input.length == 1) {
+                }else if(input.length == 1){
                     switch (input[0]) {
                         case "print":
                             System.out.println(tree);
@@ -63,14 +79,10 @@ public class MainRun {
                         case "quit":
                             quit = true;
                             System.out.println("The program is terminated.");
-                            break;
+                            return;
                         default:
                             System.out.println("Invalid input format.");
                     }
-                } else if (input.length == 3) {
-                    if (input[0].equals("search"))
-                        System.out.println(tree.Search(input[1], input[2]));
-                    else System.out.println("Invalid input format.");
                 } else System.out.println("Invalid input format.");
             }
             System.out.print("> ");
